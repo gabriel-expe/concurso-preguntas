e18f12a71b9552d757955d523e8ae1dee9a0d8ae
Gabriel Rubio <gabrielrubio983@gmail.com>
2021-10-22 14:04:37 -0500

    firebase correcion credenciales

--- src/config/credenciales.js
+++ src/config/credenciales.js
@@ -1,11 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDKpCkhoMs7siQkxFIpTjqt2d3KQKwvm4A",
-    authDomain: "mintic-4f6b7.firebaseapp.com",
-    projectId: "mintic-4f6b7",
-    storageBucket: "mintic-4f6b7.appspot.com",
-    messagingSenderId: "1070943925577",
-    appId: "1:1070943925577:web:7672c977124a8bd4a75698",
-    measurementId: "G-0CJ2QBSNSY"
-  };
+  apiKey: "AIzaSyDYUFO_9hXiR-BA8PAYzy_6KKjFC4KmlPw",
+  authDomain: "concurso-preguntas.firebaseapp.com",
+  projectId: "concurso-preguntas",
+  storageBucket: "concurso-preguntas.appspot.com",
+  messagingSenderId: "703167445855",
+  appId: "1:703167445855:web:79c78ae8a2cac2e63f14ea"
+};
 
   export default firebaseConfig
